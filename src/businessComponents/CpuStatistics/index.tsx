e945fff2abdb9c06ef8da2b541de9aaf05055f24
MaxHuang <hc18174419502@163.com>
2024-12-31 19:43:07 +0800

    business 组件封装

---
+++ src/businessComponents/CpuStatistics/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CpuStatistics() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CpuStatistics
---
+++ src/components/SubWrapper/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SubWrapper() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SubWrapper
