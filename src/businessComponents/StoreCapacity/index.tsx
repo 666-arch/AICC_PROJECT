b564eab8329216362dc673dc11676bf46ae29eff
MaxHuang <hc18174419502@163.com>
2025-01-02 19:55:14 +0800

    delete border

--- src/businessComponents/StoreCapacity/index.tsx
+++ src/businessComponents/StoreCapacity/index.tsx
@@ -1,10 +1,30 @@
 import React, { useEffect } from "react";
 import "./index.less";
 import PanelWrapper from "@/components/PanelWrapper";
+import Pie3d from "@/components/Pie3d";
+const optionsData = [
+  {
+    name: "未分配",
+    value: 260,
+    itemStyle: {
+      color: "#E9E9E9",
+    },
+  },
+  {
+    name: "已分配",
+    value: 300,
+    itemStyle: {
+      color: "#98B6FF",
+    },
+  },
+];
 function StoreCapacity() {
   return (
     <div className="main-left-store-capacity">
       <PanelWrapper width={170.5} height={27} content="储存容量" />
+      <div className='capacity-num'>9</div>
+      <Pie3d width={165} height={157} data={optionsData} left={6} top={-45}/>
+      <div className='capacity-desc'>提供100T对象存储容量</div>
     </div>
   );
 }
