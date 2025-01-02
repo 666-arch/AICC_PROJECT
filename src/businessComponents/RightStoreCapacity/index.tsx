f5721ebe2ed153cd580cea246d4fb4e4338d1c06
MaxHuang <hc18174419502@163.com>
2025-01-02 20:02:20 +0800

    pie css push

--- src/businessComponents/RightStoreCapacity/index.tsx
+++ src/businessComponents/RightStoreCapacity/index.tsx
@@ -1,9 +1,29 @@
 import PanelWrapper from '@/components/PanelWrapper'
 import './index.less'
+import Pie3d from '@/components/Pie3d'
+const optionsData = [
+  {
+    name: "未分配",
+    value: 260,
+    itemStyle: {
+      color: "#E9E9E9",
+    },
+  },
+  {
+    name: "已分配",
+    value: 300,
+    itemStyle: {
+      color: "#98B6FF",
+    },
+  },
+];
 function RightStoreCapacity() {
   return (
     <div className='main-right-store-capacity'>
       <PanelWrapper width={170.5} height={27} content="储存容量" />
+      <div className='right-capacity-num'>32</div>
+      <Pie3d width={165} height={157} data={optionsData} left={6} top={-45}/>
+      <div className='right-capacity-desc'>提供50TNAS存储容量</div>
     </div>
   )
 }
--- src/businessComponents/RightStoreSouce/index.tsx
+++ src/businessComponents/RightStoreSouce/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './index.less'
 import PanelWrapper from '@/components/PanelWrapper'
+import Pie3d from '@/components/Pie3d';
+const optionsData = [
+  {
+    name: "未分配",
+    value: 260,
+    itemStyle: {
+      color: "#E9E9E9",
+    },
+  },
+  {
+    name: "已分配",
+    value: 300,
+    itemStyle: {
+      color: "#98B6FF",
+    },
+  },
+];
 function RightStoreSouce() {
   return (
     <div className='main-right-store-source'>
       <PanelWrapper width={170.5} height={27} content="储存数据" />
+      <div className='right-source-num'>19</div>
+      <Pie3d width={165} height={157} data={optionsData} left={6} top={-45}/>
+      <div className='right-source-desc'>提供1.27P并行文件存储容量</div>
     </div>
   )
 }
