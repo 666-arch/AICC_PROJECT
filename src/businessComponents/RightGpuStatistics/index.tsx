5628b9a65a5459877d142b312c1bed5e6560e065
MaxHuang <hc18174419502@163.com>
2025-01-02 14:51:28 +0800

    right process bar config

--- src/businessComponents/RightGpuStatistics/index.tsx
+++ src/businessComponents/RightGpuStatistics/index.tsx
@@ -1,10 +1,20 @@
 import PanelWrapper from '@/components/PanelWrapper'
 import React from 'react'
 import './index.less'
+import ProcessBar from '@/components/ProcessBar'
 function RightGpuStatistics() {
   return (
     <div className='main-right-gpu-statistics'>
       <PanelWrapper width={362} height={27} content="GPU统计数据" />
+      
+      <div className="right-gpu-statistics-bar">
+        <div className="right-gpu-statistics-top">
+          <div className="right-bar-content-1">L40sGPU卡</div>
+          <div className="right-bar-content-2">84</div>
+        </div>
+        <ProcessBar width={362} height={8} value={10} />
+      </div>
+      
     </div>
   )
 }
