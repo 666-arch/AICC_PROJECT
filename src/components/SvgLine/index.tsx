c9031e051f759e6205e9fec9f97113beff922250
MaxHuang <hc18174419502@163.com>
2025-01-03 20:14:04 +0800

    position change

--- src/components/SvgLine/index.tsx
+++ src/components/SvgLine/index.tsx
@@ -10,15 +10,14 @@ function SvgLine() {
       path.style.strokeDashoffset = pathLength;
     }
   }, []);
-
   return (
     <div className="svg-anim-box">
-      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 93 260" xmlns="http://www.w3.org/2000/svg">
         <path
           className="animated-path"
           d="M10 190 L10 10 Q 10 5 15 5 L190 5"
-          stroke="black"
-          stroke-width="2"
+          stroke="#FFFFFF80"
+          stroke-width="1.5"
           fill="none"
         />
       </svg>
