025a31062d0cf322b0d443954da343b945f68a10
MaxHuang <hc18174419502@163.com>
2025-01-05 16:39:35 +0800

    update 0105

--- src/components/BtnSvgLine/index.tsx
+++ src/components/BtnSvgLine/index.tsx
@@ -3,8 +3,8 @@ import "./index.less";
 function BtnSvgLine() {
   return (
     <div className="btn-svg-line">
-      <svg width="20" height="134" xmlns="http://www.w3.org/2000/svg">
-        <line x1="10" y1="134" x2="10" y2="0" className="line" />
+      <svg width="20" height="146" xmlns="http://www.w3.org/2000/svg">
+        <line x1="10" y1="146" x2="10" y2="0" className="line" />
       </svg>
     </div>
   );
