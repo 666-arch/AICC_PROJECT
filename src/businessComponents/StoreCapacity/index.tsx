366eb794d9253593f0c6bc7da2a56236fc3d5985
MaxHuang <hc18174419502@163.com>
2025-02-05 14:37:41 +0800

    fix

--- src/businessComponents/StoreCapacity/index.tsx
+++ src/businessComponents/StoreCapacity/index.tsx
@@ -3,17 +3,34 @@ import "./index.less";
 import PanelWrapper from "@/components/PanelWrapper";
 import Pie3d from "@/components/Pie3d";
 import NumberTween from "@/components/NumberTween";
+import ChartPie3D from "@/components/ChartPie3D";
+// const optionsData = [
+//   {
+//     name: "未分配",
+//     value: 460,
+//     itemStyle: {
+//       color: "#E9E9E9",
+//     },
+//   },
+//   {
+//     name: "已分配",
+//     value: 120,
+//     itemStyle: {
+//       color: "#6a94fd",
+//     },
+//   },
+// ];
 const optionsData = [
   {
     name: "未分配",
-    value: 460,
+    value: 55.15,
     itemStyle: {
       color: "#E9E9E9",
     },
   },
   {
     name: "已分配",
-    value: 120,
+    value: 44.85,
     itemStyle: {
       color: "#6a94fd",
     },
@@ -24,7 +41,8 @@ function StoreCapacity() {
     <div className="main-left-store-capacity">
       <PanelWrapper width={170.5} height={27} content="储存容量" />
       <div className='capacity-num'><NumberTween value={9}/></div>
-      <Pie3d width={165} height={157} data={optionsData} left={6} top={-45}/>
+      {/* <Pie3d width={165} height={157} data={optionsData} left={6} top={-45}/> */}
+      <ChartPie3D width={165} height={157} data={optionsData} left={6} top={-45}/>
       <div className="pie-base-bg"></div>
       <div className='capacity-desc'>提供100T对象存储容量</div>
     </div>
